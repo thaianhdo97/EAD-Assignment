221a0104ad9089bb0ed7dc869b8b25d4acacd62b
thaianh97 <58674701+thaianh97@users.noreply.github.com>
2021-06-28 20:23:07 +0700

    upload project

---
+++ src/main/java/com/example/eadassignment/EadAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.eadassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eadassignment/EadAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eadassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
